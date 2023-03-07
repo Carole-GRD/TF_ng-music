95a7f91590b48f981dfed223ac62cd7208710c7f
Carole Gérard <gerard.carole@skynet.be>
2023-03-07 11:54:48 +0100

    modules + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MusicComponent } from './music/music.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MusicComponent /* TODO lazyloading children */ },
+  { path: 'admin', component: AdminComponent /* TODO lazyloading children */ },
+  { path: 'not-found', component: NotFoundComponent },
+  // Attention à toujours mettre route ** en dernier ->
+    // Correspond à "Si aucun chemin précédemment défini" donc doit être en dessous de tous les chemins
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
