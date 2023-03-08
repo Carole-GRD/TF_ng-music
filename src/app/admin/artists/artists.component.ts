a2517ae1995b7f6be9075160aad9e07cac8a80cc
Carole Gérard <gerard.carole@skynet.be>
2023-03-08 11:00:52 +0100

    display all artists ok

--- src/app/admin/artists/artists.component.ts
+++ src/app/admin/artists/artists.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Artist } from 'src/app/shared/models/artist';
+import { ArtistService } from 'src/app/shared/services/artist/artist.service';
 
 @Component({
   selector: 'app-artists',
   templateUrl: './artists.component.html',
   styleUrls: ['./artists.component.scss']
 })
-export class ArtistsComponent {
+export class ArtistsComponent implements OnInit{
+
+
+  listArtists : Artist[] = [];
+
+  constructor(private _artistService : ArtistService) {}
+
+  ngOnInit(): void {
+    this._artistService.getAll().subscribe({
+      next : (res) => {
+        console.log("NEXT", res);
+        this.listArtists = res.results;
+      },
+      error : (err) => {
+        console.log("ERROR", err);
+      },
+      complete : () => {
+        console.log("COMPLETE");
+      }
+    })
+  }
 
 }
