9ee3072ed2cba184dae555f7dfc50aad24d8c6b3
Carole Gérard <gerard.carole@skynet.be>
2023-03-09 16:37:24 +0100

    album create

--- src/app/admin/albums/create-album/create-album.component.ts
+++ src/app/admin/albums/create-album/create-album.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlbumService } from 'src/app/shared/services/album/album.service';
 
 @Component({
   selector: 'app-create-album',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class CreateAlbumComponent {
 
+  albumForm : FormGroup;
+
+  constructor(
+    private _fb : FormBuilder,
+    private _albumService : AlbumService,
+    private _router : Router
+  ) {
+    this.albumForm = this._fb.group({
+      title : [null, [Validators.required, Validators.maxLength(50)]]
+    })
+  }
+
+  createAlbum() : void {
+    if (this.albumForm.valid) {
+      this._albumService.create(this.albumForm.value).subscribe({
+        next : (res) => {},
+        error : (err) => {},
+        complete : () => {
+          this._router.navigateByUrl('/admin/albums')
+        }
+      })
+    }  
+    else {
+      this.albumForm.markAllAsTouched();
+    }
+  }
 }
+
